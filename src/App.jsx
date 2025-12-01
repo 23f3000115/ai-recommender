0da737577306411a466d4743b55edfa16c63c561
Sahilpreet Singh <23f3000115@ds.study.iitm.ac.in>
2025-12-01 14:18:24 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,3 @@
-
+import React from "react";
+import AIProductRecommender from "./components/AIProductRecommender";
+export default function App() {  return (    <div className="min-h-screen bg-slate-50 p-6">      <AIProductRecommender />    </div>  );}
